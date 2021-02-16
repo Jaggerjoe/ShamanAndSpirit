08f3271aa0cada92e9e45d1db850f156ea62768b
JaggerJoe <gregoire.thomassin@e-artsup.net>
2021-02-16 12:58:45 +0100

    Add Input, Movement, followCamera
    Ajout de l'input manager, création du bindings des inputs de script de deplacement, et du follow de la camera

---
+++ ShamanAndSpirit/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private SO_PlaterController m_InputController = null;
+
+    [SerializeField]
+    private float m_Speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(m_InputController.Movement, Time.deltaTime);
+    }
+
+    private void Movement(Vector2 p_Movement, float p_DeltaTime)
+    {
+        transform.position += new Vector3(p_Movement.x, 0, p_Movement.y) * m_Speed * p_DeltaTime;
+    }
+}
