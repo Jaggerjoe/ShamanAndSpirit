c7f096012d77b69df0d34baf2dc468438c3bed42
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2021-02-25 19:06:45 +0100

    Ajout des 2 ennemis (comportements) + Spawner

---
+++ ShamanAndSpirit/Assets/Marvyn/Enemy.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target = null;
+
+    [SerializeField]
+    private float m_AttackRange;
+    [SerializeField]
+    private float m_Speed;
+
+    [SerializeField]
+    float m_AttackRate = 0;
+
+    private bool CanMove = true;
+    private bool CanAttack = true;
+
+    [SerializeField]
+    private LayerMask m_MaskToDetect;
+
+    public void Start()
+    {
+        m_Target = FindObjectOfType<PlayerManager>().transform;
+    }
+
+    public void Move()
+    {
+        if(CanMove == true)
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, m_Target.transform.position, (m_Speed * Time.deltaTime));
+
+        }
+        
+    }
+
+    public void AttackRange()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, m_AttackRange, m_MaskToDetect);
+
+        if(hitColliders.Length >= 1)
+        {
+            foreach (Collider collider in hitColliders)
+            {
+
+                Attack();
+
+
+            }
+        }
+        else
+        {
+            CanMove = true;
+        }
+        
+        
+    }
+
+    public void Attack()
+    {
+        CanMove = false;
+        
+
+        if (m_AttackRate <= 1.5)
+        {
+            m_AttackRate += Time.deltaTime;
+            
+        }
+        else
+        {
+            m_AttackRate = 0;
+            Debug.Log("hit");
+
+        }
+    }
+
+    private void Update()
+    {
+        Move();
+        AttackRange();
+    }
+
+    private void GetStunned()
+    {
+        CanAttack = false;
+        CanMove = false;
+    }
+
+    private void GetUnStunned()
+    {
+        CanAttack = true;
+        CanMove = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, m_AttackRange);
+    }
+}
---
+++ ShamanAndSpirit/Assets/Marvyn/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private float m_TimeBeforeSpawn;
+    private float m_CurrentTimeBeforeSpawn;
+
+    [SerializeField]
+    private GameObject m_EnemyToInstantiate;
+
+    [SerializeField]
+    private int m_NbEnemyToSpawn;
+    private int m_NbEnemySpawned;
+
+    private void Update()
+    {
+        if(m_CurrentTimeBeforeSpawn <= m_TimeBeforeSpawn)
+        {
+            m_CurrentTimeBeforeSpawn += Time.deltaTime;
+        }
+        else
+        {
+            m_CurrentTimeBeforeSpawn = 0;
+
+            if(m_NbEnemySpawned < m_NbEnemyToSpawn)
+            {
+                Instantiate(m_EnemyToInstantiate, transform.position, Quaternion.identity);
+                m_NbEnemySpawned += 1;
+            }
+            
+        }
+    }
+}
