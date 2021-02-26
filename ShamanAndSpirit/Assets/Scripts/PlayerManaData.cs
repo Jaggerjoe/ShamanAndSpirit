b5d003c3487bbd8e21b09013578a0e6f92aac4a1
And4Code <Andrea.bois@e-artsup.net>
2021-02-27 00:47:05 +0100

    transfert Mana de la manière plus simple possible

---
+++ ShamanAndSpirit/Assets/Scripts/PlayerManaData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManaData : MonoBehaviour
+{
+   public float m_MaxMana;
+
+   public float m_CurrentMana;
+
+    public float MaxMana => m_MaxMana;
+    public float CurrentMana => m_CurrentMana;
+
+    private void Start()
+    {
+        m_MaxMana = 100;
+        m_CurrentMana = 40;
+    }
+}
